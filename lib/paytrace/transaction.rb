48454f58d28d4390ddd275157b09a2a5d414f132
Trevor Redfern <trevorredfern@gmail.com>
2014-02-27 16:57:40 -0800

    Tracks transaction types now

--- lib/paytrace/transaction.rb
+++ lib/paytrace/transaction.rb
@@ -1,7 +1,9 @@
 module PayTrace
   module TransactionOperations
     def sale(amount: nil, credit_card: nil, options: {})
-      Transaction.new(amount: amount, credit_card: credit_card)
+      Transaction.new(amount: amount, 
+                      credit_card: credit_card, 
+                      type: TransactionTypes::SALE)
     end
   end
 
@@ -10,11 +12,16 @@ module PayTrace
       include TransactionOperations
     end
 
-    attr_reader :amount, :credit_card
+    attr_reader :amount, :credit_card, :type
 
-    def initialize(amount: nil, credit_card: nil)
+    def initialize(amount: nil, credit_card: nil, type: nil)
       @amount = amount
       @credit_card = CreditCard.new(credit_card)
+      @type = type
     end
+  end
+
+  module TransactionTypes
+    SALE = "SALE"
   end
 end
--- test/paytrace/transaction_spec.rb
+++ test/paytrace/transaction_spec.rb
@@ -9,8 +9,15 @@ describe PayTrace::Transaction do
         expiration_month: 10,
         expiration_year:  24
     })
+
+    #Transaction is properly configured
     t.amount.must_equal "1242.32"
+    t.type.must_equal PayTrace::TransactionTypes::SALE
+
+    #Sets up a card
     t.credit_card.card_number.must_equal "1234123412341234"
+    t.credit_card.expiration_month.must_equal 10
+    t.credit_card.expiration_year.must_equal 24
 
   end
 end
