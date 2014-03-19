1ec806324a6c9f24862eac1cf6c9e474ff945c23
ahatch1490 <ahatch1490@gmail.com>
2014-03-19 14:07:21 -0700

    updated response_code to handle multiple erros

--- lib/paytrace/api/response.rb
+++ lib/paytrace/api/response.rb
@@ -1,13 +1,30 @@
 module PayTrace
   module API
     class Response
-      attr_reader :values
+      attr_reader :values, :errors
 
       def initialize(response_string)
         @field_delim = "|"
         @value_delim = "~"
         @values = {}
-        
+        @errors = {}
+        parse_response(response_string)
+      end
+
+      def response_code
+        get_response
+      end
+
+      def has_errors?
+           @errors.length > 0
+      end
+
+      def parse_response(response_string)
+
+        if (response_string.include? "ERROR")
+           return parse_errors(response_string)
+        end
+
         pairs = response_string.split(@field_delim)
         pairs.each do |p|
           k,v = p.split(@value_delim)
@@ -15,8 +32,33 @@ module PayTrace
         end
       end
 
-      def response_code
+      def parse_errors(response_string)
+        pairs = response_string.split(@field_delim)
+        pairs.each do |p|
+          k,v = p.split(@value_delim)
+          k = generate_error_key(k,v)
+          @errors[k] = v
+        end
+      end
+
+      def generate_error_key(key,value)
+        #get the error number from the value
+        return key +'-'+ value[/([1-9]*)/,1]
+      end
+
+      def get_response()
+        if has_errors?
+          return get_error_response()
+        end
         @values["RESPONSE"]
+      end
+
+      def get_error_response()
+        error_message = ""
+        @errors.each do  |k,v|
+          error_message << v + ","
+        end
+        error_message
       end
     end
   end
--- test/paytrace/api/response_spec.rb
+++ test/paytrace/api/response_spec.rb
@@ -7,4 +7,25 @@ describe PayTrace::API::Response do
     response = PayTrace::API::Response.new(from_server)        
     response.response_code.must_equal "101. Your transaction was successfully approved."
   end
+
+  it "parses multiple error responses" do
+    from_server ="ERROR~35. Please provide a valid Credit Card Number.|ERROR~43. Please provide a valid Expiration Month.|"
+    response = PayTrace::API::Response.new(from_server)
+    response.has_errors?.must_equal true
+  end
+
+  it "will can contain multiple error messages" do
+    from_server ="ERROR~35. Please provide a valid Credit Card Number.|ERROR~43. Please provide a valid Expiration Month.|"
+    response = PayTrace::API::Response.new(from_server)
+    response.errors.length.must_equal 2
+  end
+
+  it "should create a response with all errors in it" do
+    from_server ="ERROR~35. Please provide a valid Credit Card Number.|ERROR~43. Please provide a valid Expiration Month.|"
+    response = PayTrace::API::Response.new(from_server)
+    actual ="35. Please provide a valid Credit Card Number.,43. Please provide a valid Expiration Month.,"
+    response.response_code.must_equal actual
+  end
+
+
 end
