79267e5dec446ee2bbd3c66d42e4eaa83b2abd4a
Tom Canham <tomc@ricksteves.com>
2014-04-28 08:48:15 -0700

    Adds integration test for the SettleTranx SDK method. I couldn't test it because the dev servers are acting strangely. Bumps the version number.

---
+++ test/scripts/run_settle_transaction.rb
@@ -0,0 +1,18 @@
+$:<< "./lib" # uncomment this to run against a Git clone instead of an installed gem
+
+require "paytrace"
+require "paytrace/debug"
+
+PayTrace::Debug.configure_test
+
+# settle a transaction via a recurrence ID
+PayTrace::Debug.trace do
+  params = { recur_id: "1143" } # you must replace this with a valid recurrence ID!
+  PayTrace::Transaction.settle_transaction(params)
+end
+
+# settle a transaction via a customer ID
+PayTrace::Debug.trace do
+  params = { customer_id: "1143" } # you must replace this with a valid customer ID!
+  PayTrace::Transaction.settle_transaction(params)
+end
