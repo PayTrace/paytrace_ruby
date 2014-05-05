545ba12efd5a0298635f1c9e2281fd0797aa82ac
Tom Canham <tom@paytrace.com>
2014-05-05 07:30:07 -0700

    Expanded the integration test a bit more. Assuming you can come up with valid customer/check ID's, this puts it through more of its paces.

--- test/scripts/run_check_transactions.rb
+++ test/scripts/run_check_transactions.rb
@@ -3,11 +3,15 @@ $:<< "./lib" # uncomment this to run against a Git clone instead of an installed
 require "paytrace"
 require "paytrace/debug"
 
+# see: http://help.paytrace.com/api-processing-a-check-sale
+
 # change this as needed to reflect the username, password, and test host you're testing against
 PayTrace::Debug.configure_test("demo123", "demo123", "stage.paytrace.com")
 
-# http://help.paytrace.com/api-processing-a-check-sale
+# setting this to false keeps the framework from throwing an exception and halting the integration test at the first error
+PayTrace::API::Gateway.raise_exceptions = false
 
+# process sale transaction -- replace with valid customer ID to see it succeed.
 PayTrace::Debug.trace do
   params = {
   check_type: "Sale",
@@ -18,4 +22,58 @@ PayTrace::Debug.trace do
 }
 
   PayTrace::CheckTransaction::process_sale(params)
+end
+
+# process manage check -- this is invalid data, you can replace with a valid check number 
+# for the login to see it succeed.
+PayTrace::Debug.trace do
+  params = {
+    check_type: "Hold",
+    check_id: 1234
+  }
+
+  PayTrace::CheckTransaction::manage_check(params)
+end
+
+# process a check refund -- replace this with a valid customer ID and amount to see it succeed
+PayTrace::Debug.trace do
+  ba = PayTrace::Address.new({
+    name: "John Doe",
+    street: "1234 Main Street",
+    street2: "Apartment 1B",
+    city: "Shoreline",
+    state: "WA",
+    country: "US",
+    postal_code: "98133",
+    address_type: :billing
+  })
+
+  sa = PayTrace::Address.new({
+    name: "Jane Doe",
+    street: "1235 Moon Street",
+    street2: "Apartment 2C",
+    city: "Shortline",
+    state: "WA",
+    country: "US",
+    postal_code: "98134",
+    address_type: :shipping
+  })
+
+  params = {
+    check_type: "Refund",
+    amount: 15.99,
+    customer_id: "MMouse",
+    billing_address: ba,
+    shipping_address: sa,
+    email: "tom@paytrace.com",
+    invoice: "abc1234",
+    description: "accidental billing",
+    tax: 2.99,
+    customer_reference_id: '1234AB',
+    discretionary_data: {
+      hair_color: :red
+    }
+  }
+
+  PayTrace::CheckTransaction::process_refund(params)
 end
