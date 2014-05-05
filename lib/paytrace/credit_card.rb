e774709670694cfcd0b54004d5dab5c168e09f5e
Tom Canham <tom@paytrace.com>
2014-05-05 08:00:37 -0700

    Docs for CreditCard class.

--- lib/paytrace/credit_card.rb
+++ lib/paytrace/credit_card.rb
@@ -1,7 +1,15 @@
 module PayTrace
+  # Contains credit card information, including possible swipe data.
   class CreditCard
     attr_accessor :card_number, :expiration_month, :expiration_year, :swipe, :csc
 
+    # Initialize instance; possible options are: 
+    # * *card_number* -- the credit card number
+    # * *expiration_month* -- the expiration month of the card
+    # * *expiration_year* -- the expiration year of the card
+    # * *csc* -- the CSC code
+    # * *swipe* -- swipe data, if present
+    # All parameters are passed in a hash, with symbols for key names.
     def initialize(options={})
 
       @card_number = options[:card_number]
