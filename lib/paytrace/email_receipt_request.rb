6ae89c6bd5356eecbc20240e25c861abdbb58bf6
Tom Canham <tom@paytrace.com>
2014-05-05 12:54:00 -0700

    Adds docs to email receipt request object.

--- lib/paytrace/email_receipt_request.rb
+++ lib/paytrace/email_receipt_request.rb
@@ -1,8 +1,16 @@
 module PayTrace
+  # Methods to request an email receipt for a transaction
   class EmailReceiptRequest
+    # :nodoc:
     TRANSACTION_METHOD = "EmailReceipt"
     attr_accessor :email, :transaction_id, :check_id
+    # :doc:
 
+    # Initializer. Params:
+    # *:email* -- the email address to send the receipt to
+    # *:transaction_id* -- the transaction ID of the transaction to email
+    # *:check_id* -- the check ID of the transaction to email
+    # _Note:_ only use *:transaction_id* _or_ *:check_id* -- not both.
     def initialize(params = {})
       email, id, id_is_check_id = false
       @email = params[:email]
@@ -10,6 +18,7 @@ module PayTrace
       @check_id = params[:check_id]
     end
 
+    # :nodoc:
     def set_request(request = nil)
       request ||= PayTrace::API::Request.new
       request.set_param(:method, TRANSACTION_METHOD)
@@ -19,7 +28,9 @@ module PayTrace
 
       request
     end
+    # :doc:
 
+    # Sends the request for the transaction. Accepts an existing request object, or creates one if needed.
     def send_request(request = nil)
       request ||= set_request
 
