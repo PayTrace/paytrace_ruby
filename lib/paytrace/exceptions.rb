e5a2c26b7a3ec2f804a5ff6eeb335aec46a69124
Tom Canham <tom@paytrace.com>
2014-05-05 12:54:15 -0700

    Adds docs to exceptions module.

--- lib/paytrace/exceptions.rb
+++ lib/paytrace/exceptions.rb
@@ -1,11 +1,15 @@
 module PayTrace
+  # Container module for custom PayTrace exceptions
   module Exceptions
+    # Base PayTrace exception object
     class Base < RuntimeError
     end
 
+    # Raised when an unknown field is sent, or an incorrect value is used
     class ValidationError < Base
     end
 
+    # Raised by default when an error response is returned from the server
     class ErrorResponse < Base
     end
   end
