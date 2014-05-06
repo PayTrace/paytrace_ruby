5b9fa5d759743acf1758d48d84697351ba353289
Tom Canham <tom@paytrace.com>
2014-05-06 14:50:20 -0700

    Docs for the Response object.

--- lib/paytrace/api/response.rb
+++ lib/paytrace/api/response.rb
@@ -4,6 +4,7 @@ module PayTrace
     class Response
       attr_reader :values, :errors
 
+      # Called by the PayTrace::API::Gateway object to initialize a response
       def initialize(response_string, multi_value_fields = [])
         @field_delim = "|"
         @value_delim = "~"
@@ -13,14 +14,17 @@ module PayTrace
         parse_response(response_string, multi_value_fields)
       end
 
+      # Returns the response code(s) received
       def response_code
         get_response
       end
 
+      # Returns true if the response contained any error codes
       def has_errors?
-           @errors.length > 0
+        @errors.length > 0
       end
 
+      # Called by the initialize method
       def parse_response(response_string, multi_value_fields = [])
 
         if (response_string.include? "ERROR")
@@ -39,6 +43,7 @@ module PayTrace
         end
       end
 
+      # Called by the framework in the event of an error response
       def parse_errors(response_string)
         pairs = response_string.split(@field_delim)
         pairs.each do |p|
@@ -48,11 +53,13 @@ module PayTrace
         end
       end
 
+      # Internal use only
       def generate_error_key(key,value)
         #get the error number from the value
         return key +'-'+ value[/([1-9]*)/,1]
       end
 
+      # Returns any status code(s) or error code(s) received
       def get_response()
         if has_errors?
           return get_error_response()
@@ -60,6 +67,7 @@ module PayTrace
         @values["RESPONSE"]
       end
 
+      # Returns any error code(s) received
       def get_error_response()
         error_message = ""
         @errors.each do  |k,v|
