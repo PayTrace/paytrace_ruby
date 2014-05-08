692ca117737949f0999586d4b370b0b64fc59784
Tom Canham <tom@paytrace.com>
2014-05-08 13:36:05 -0700

    Dramatically reduce complexity and repeated code in batch_operations.rb by utilizing the Gateway.send_request static method.

--- lib/paytrace/batch_operations.rb
+++ lib/paytrace/batch_operations.rb
@@ -13,15 +13,7 @@ module PayTrace
     #
     # * *:batch_number* -- number of the batch of transactions you wish to export
     def self.exportSingle(params = {})
-      request = PayTrace::API::Request.new
-      request.set_param(:method, EXPORT_SINGLE_METHOD)
-      request.set_param(:batch_number, params[:batch_number])
-
-      gateway = PayTrace::API::Gateway.new
-      response = gateway.send_request(request)      
-      unless response.has_errors?
-        response.values
-      end
+      PayTrace::API::Gateway.send_request(EXPORT_SINGLE_METHOD, [:batch_number], params)
     end
 
     # See http://help.paytrace.com/api-export-batches
@@ -31,15 +23,7 @@ module PayTrace
     # * *:start_date* -- indicates when to start searching for transactions to export. Must be a valid date formatted as MM/DD/YYYY
     # * *:end_date* -- indicates when to end searching for transactions to export. Must be a valid date formatted as MM/DD/YYYY
     def self.exportMultiple(params = {})
-      request = PayTrace::API::Request.new
-      request.set_param(:method, EXPORT_MULTIPLE_METHOD)
-      request.set_params([:start_date, :end_date], params)
-
-      gateway = PayTrace::API::Gateway.new
-      response = gateway.send_request(request)      
-      unless response.has_errors?
-        response.values
-      end
+      PayTrace::API::Gateway.send_request(EXPORT_MULTIPLE_METHOD, [:start_date, :end_date], params)
     end
 
     # See http://help.paytrace.com/api-export-batch-details
@@ -48,15 +32,7 @@ module PayTrace
     #
     # * *:batch_number* -- number of the batch of transactions you wish to export
     def self.exportDetails(params = {})
-      request = PayTrace::API::Request.new
-      request.set_param(:method, EXPORT_DETAILS_METHOD)
-      request.set_param(:batch_number, params[:batch_number])
-
-      gateway = PayTrace::API::Gateway.new
-      response = gateway.send_request(request)      
-      unless response.has_errors?
-        response.values
-      end
+      PayTrace::API::Gateway.send_request(EXPORT_DETAILS_METHOD, [:batch_number], params)
     end
   end
 end
