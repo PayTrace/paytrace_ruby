16438dce5b2a442c73a148474bd02193e8cf8ecc
Tom Canham <tom@paytrace.com>
2014-05-13 13:48:50 -0700

    Adding test script to show that settle_transactions doesn't need parameters.

--- lib/paytrace/version.rb
+++ lib/paytrace/version.rb
@@ -1,4 +1,4 @@
 module PayTrace
   # Version number
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
--- test/scripts/run_settle_transaction.rb
+++ test/scripts/run_settle_transaction.rb
@@ -6,14 +6,7 @@ require "paytrace/debug"
 # change this as needed to reflect the username, password, and test host you're testing against
 PayTrace::Debug.configure_test("demo123", "demo123", "stage.paytrace.com")
 
-# settle a transaction via a recurrence ID
-PayTrace::Debug.trace do
-  params = { recur_id: "1143" } # you must replace this with a valid recurrence ID!
-  PayTrace::Transaction.settle_transactions(params)
-end
-
 # settle a transaction via a customer ID
 PayTrace::Debug.trace do
-  params = { customer_id: "1143" } # you must replace this with a valid customer ID!
-  PayTrace::Transaction.settle_transactions(params)
+  PayTrace::Transaction.settle_transactions()
 end
