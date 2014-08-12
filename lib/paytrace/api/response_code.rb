b22bb568d1b9d3fd455b2f8aab9922dfa8b28946
John Quach <john@paytrace.com>
2014-08-12 13:17:15 -0700

    check if there is already a class created

--- lib/paytrace/api/response_code.rb
+++ lib/paytrace/api/response_code.rb
@@ -1,17 +1,40 @@
 module PayTrace
   module API
-    class ResponseCode
-      def self.define(code,text)
+
+    class Factory
+      def self.create(klass_name, code, text)
         klass = Class.new do
           def initialize(text)
-	    @text = text
+            @text = text
           end
           attr_reader :text
         end
 
-        klass_name = CodeDefinitions[code.to_sym]
-        new_klass = self.const_set(klass_name, klass)
+        new_klass = ResponseCode.const_set(klass_name, klass)
         new_klass.new(text)
+      end
+    end
+
+    class ResponseCode
+      @@factory = PayTrace::API::Factory
+
+      def self.set_class_factory(factory)
+        @@factory = factory
+      end
+
+      def self.define(code,text)
+        if CodeDefinitions.has_key? code.to_sym
+          klass_name = CodeDefinitions[code.to_sym]
+        else
+          klass_name = "DefaultResponse"
+        end
+        
+        if (ResponseCode.const_defined? klass_name)
+          klass = ResponseCode.const_get(klass_name)
+          klass.new(text)
+        else
+          @@factory.create(klass_name, code, text)
+        end
       end
 
       CodeDefinitions = {
